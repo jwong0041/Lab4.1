923a141f619a3405e4e63f74b242322a2cf193e9
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-03-14 09:29:46 -0400

    Smash Mouth

--- Lab4.1/src/Cards.java
+++ Lab4.1/src/Cards.java
@@ -1,9 +1,15 @@
 public class Cards 
 {
-
-	public Cards() 
+	private String rank;
+	private String suit;
+	private int value;
+	
+	public Cards(String rank, String suit, int value) 
 	{
 		// TODO Auto-generated constructor stub
+		this.rank = rank;
+		this.suit = suit;
+		this.value = value;
 	}
 
 }
--- Lab4.1/src/Deck.java
+++ Lab4.1/src/Deck.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Deck 
 {
-
+	ArrayList <Card> unDealt;
+	public Deck(String[] rank, String[] suit, String[] value)
+	{
+		for(int i = 0; i<rank.length; i++)
+		{
+			for(int j = 0; j<suit.length; j++)
+			{
+				unDealt.add(new Card(rank[i], suit[j], value[i]));
+			}
+		}
+	}
 }
