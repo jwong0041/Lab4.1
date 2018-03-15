689f87b4030f73ed8fd720b0e2ea9adc4a6446d2
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-03-15 09:22:54 -0400

    Shooting Stars

--- Lab4.1/src/Cards.java
+++ Lab4.1/src/Cards.java
@@ -1,9 +1,36 @@
 public class Cards 
 {
+	//fields
 	private String rank;
 	private String suit;
 	private int value;
+
+	//Getters n' Setters
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 	
+	//constructor
 	public Cards(String rank, String suit, int value) 
 	{
 		// TODO Auto-generated constructor stub
@@ -11,5 +38,20 @@ public class Cards
 		this.suit = suit;
 		this.value = value;
 	}
-
+	
+	//check to see if otherCard is equal to Card
+	public boolean equals(Cards otherCard)
+	{
+		if (otherCard.getValue() == this.value)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	//make card printable. 
+	public String toString()
+	{
+		return this.rank + " of " + this.suit + ", current value: " + this.value;
+	}
 }
--- Lab4.1/src/Deck.java
+++ Lab4.1/src/Deck.java
@@ -2,15 +2,45 @@ import java.util.ArrayList;
 
 public class Deck 
 {
-	ArrayList <Card> unDealt;
-	public Deck(String[] rank, String[] suit, String[] value)
+	//fields, do they need to be public/private? or is this alright?
+	ArrayList<Cards> unDealt;
+	ArrayList<Cards> Dealt;
+	
+	//Constructor
+	public Deck(String[] rank, String[] suit, int[] value)
 	{
 		for(int i = 0; i<rank.length; i++)
 		{
 			for(int j = 0; j<suit.length; j++)
 			{
-				unDealt.add(new Card(rank[i], suit[j], value[i]));
+				unDealt.add(new Cards(rank[i], suit[j], value[i]));
 			}
 		}
 	}
+	
+	//checking to see if the undealt deck is empty, just like my wallet
+	public boolean isEmpty()
+	{
+		if(unDealt.size() == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	//size of the deck ;)))
+	public int size()
+	{
+		return this.unDealt.size();
+	}
+	
+	//dealing cards [move from undealt to dealt, return Card]
+	public String deal()
+	{
+		Cards r = unDealt.remove(0);
+		Dealt.add(r);
+		return r.toString();
+	}
+	
+	
 }
